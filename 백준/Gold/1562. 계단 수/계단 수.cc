dc31aa262f2140d1c472ad8a310daa6eb5a92ce0
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-01-17 03:39:45 +0900

    [Gold I] Title: 계단 수, Time: 8 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -6,26 +6,36 @@ const int MOD = 1'000'000'000;
 
 int n;
 ll dp[101][10][1 << 10]; // [길이][마지막 숫자][비트마스크]
+ll res;
 
-void input() {
-    cin >> n;
+void input();
+void func();
+void output();
+
+
+int main() {
+    input();
+    func();
+    output();
+    return 0;
 }
 
+
+
 void func() {
-    // 초기값 설정: 길이 1에서 각 숫자로 끝나는 경우
     for (int i = 1; i <= 9; i++) {
-        dp[1][i][1 << i] = 1; // 숫자 i만 사용됨
+        dp[1][i][1 << i] = 1; 
     }
 
     // dp 진행
     for (int i = 2; i <= n; i++) { // 길이
         for (int j = 0; j < 10; j++) { // 마지막 숫자
             for (int mask = 0; mask < (1 << 10); mask++) { // 비트마스크
-                if (j > 0) { // j-1로 끝나는 경우
+                if (j > 0) {
                     dp[i][j][mask | (1 << j)] += dp[i - 1][j - 1][mask];
                     dp[i][j][mask | (1 << j)] %= MOD;
                 }
-                if (j < 9) { // j+1로 끝나는 경우
+                if (j < 9) {
                     dp[i][j][mask | (1 << j)] += dp[i - 1][j + 1][mask];
                     dp[i][j][mask | (1 << j)] %= MOD;
                 }
@@ -34,17 +44,18 @@ void func() {
     }
 
     // 결과 계산
-    ll res = 0;
+    res = 0;
     for (int j = 0; j < 10; j++) {
         res += dp[n][j][(1 << 10) - 1]; // 0~9 모든 숫자를 사용한 경우
         res %= MOD;
     }
-
-    cout << res << '\n';
 }
 
-int main() {
-    input();
-    func();
-    return 0;
+
+void input() {
+    cin >> n;
+}
+
+void output() {
+    cout<<res<<'\n';
 }
