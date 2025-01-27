924a6f72a73b12b58082877ada5eeca09c73de26
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-01-28 01:01:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c, d, e, f;
+int aa, bb, cc, dd, ee, ff;
+
+int main(){
+    a = 1; b = 1; c = 2; d = 2; e = 2; f = 8;
+    cin>>aa>>bb>>cc>>dd>>ee>>ff;
+    cout<<-aa+a<<' '<<-bb+b<<' '<<-cc+c<<' '<<-dd+d<<' '<<-ee+e<<' '<<-ff+f<<'\n';
+    return 0;
+}
