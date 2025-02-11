2ba9a42a2605ff50d64d42b0c1a35eafc060580d
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-02-11 13:33:47 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string res;
+    int cnt = 0;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] == '.'){
+            if(cnt % 4 == 0){
+                int loop = cnt / 4;
+                for(int j=0; j<loop; j++){
+                    res += "AAAA";
+                }
+                res += ".";
+            }
+            else if(cnt % 2 == 0){
+                int loop = cnt / 4;
+                for(int j=0; j<loop; j++){
+                    res += "AAAA";
+                }
+                res += "BB.";
+            }
+            else{
+                res = "-1";
+                cnt = 0;
+                break;
+            }
+            cnt = 0;
+        }
+        else{
+            cnt++;
+        }
+    }
+
+    if(cnt != 0){
+        if(cnt % 4 == 0){
+            int loop = cnt / 4;
+            for(int j=0; j<loop; j++){
+                res += "AAAA";
+            }
+        }
+        else if(cnt % 2 == 0){
+            int loop = cnt / 4;
+            for(int j=0; j<loop; j++){
+                res += "AAAA";
+            }
+            res += "BB";
+        }
+        else{
+            res = "-1";
+        }
+    }
+
+    cout<<res<<'\n';
+        
+    return 0;
+}
