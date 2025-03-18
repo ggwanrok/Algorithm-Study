0d26b6f46840341702f55685f3c184ee54a37585
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-03-18 16:44:13 +0900

    [Silver V] Title: 소수, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    ll a, b, n;
+    cin>>a>>b>>n;
+
+    while(n--){
+        a %= b;
+        a *= 10;
+    }
+    cout<<(a/b)%10<<'\n';
+}
