6f9e33ce7ef884c8f68983efa0caee858cb2da93
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-06-24 01:31:47 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,20 @@
+// start : 12:55
+/*
+a <= b : yes
+a > b : 소인수 분해한 합이 b 이하라면 가능.
+
+근데 -1 까지 고려하면 그냥 언제나 yes인 건가?? ㅋㅋㅋ
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int a, b;
+        cin>>a>>b;
+        cout<<"yes\n";
+    }
+    return 0;
+}
