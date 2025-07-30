043281a055fadcd1d401e6a1b63da64112eb9871
gwanrok <145204556+moonditation@users.noreply.github.com>
2025-07-31 03:12:47 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    set<int> s;
+    for(int i=0; i<n; i++){
+        int a; cin>>a;
+        s.insert(a);
+    }
+    for(int cur : s){
+        cout<<cur<<' ';
+    }
+    cout<<'\n';
+    return 0;
+}
