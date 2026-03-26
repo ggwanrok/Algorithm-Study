a1e26025e65b7cb49396c09af97d50475b3f37c5
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-03-26 16:56:59 +0900

    [level 2] Title: 완전범죄, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -1,37 +1,40 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define INF 1000000000
 using namespace std;
 
-const int INF = 1e9;
+int solution(vector<vector<int>> info, int n, int m) {
+    vector<int> dp(m, INF);
+    dp[0] = 0;
 
-int L, N, M;
-vector<vector<int>> info;
-int memo[41][121][121];
+    for (auto cur : info) {
+        int da = cur[0];
+        int db = cur[1];
 
-int dfs(int idx, int a, int b) {
-    if (a >= N || b >= M) return INF;
-    if (idx == L) return 0;
+        vector<int> next(m, INF);
+        /*각 cur에 대해서,
+        b가 i 만큼 훔쳤을 때, 가지고 있는 a가 훔친 정보의 최솟값이 dp에 저장되어 있다.
+        직전까지 고려한 dp를 기반으로 현시점 훔침행동을 통해서 업데이트 할 수 있는 정보를 next에 저장
+        반드시 step이 진행되어야 하기에 dp를 기반으로 처리
+        이후 dp에 덮어쓰기
+        */
+        for (int i = 0; i < m; i++) {
+            if(dp[i] == INF) continue;
+            if(dp[i] + da < n){
+                next[i] = min(next[i], dp[i] + da);
+            }
+            if(i + db < m){
+                next[i+db] = min(next[i+db], dp[i]);
+            }
+        }
+        dp = move(next);
+    }
 
-    int &ret = memo[idx][a][b];
-    if (ret != -1) return ret;
+    int answer = INF;
+    for (int i = 0; i < m; i++) {
+        answer = min(answer, dp[i]);
+    }
 
-    int da = info[idx][0];
-    int db = info[idx][1];
-
-    ret = INF;
-    ret = min(ret, da + dfs(idx + 1, a + da, b));   // A가 훔침
-    ret = min(ret, dfs(idx + 1, a, b + db));        // B가 훔침
-
-    return ret;
-}
-
-int solution(vector<vector<int>> _info, int n, int m) {
-    info = _info;
-    N = n;
-    M = m;
-    L = info.size();
-
-    memset(memo, -1, sizeof(memo));
-
-    int ans = dfs(0, 0, 0);
-    return (ans >= INF ? -1 : ans);
+    return answer == INF ? -1 : answer;
 }
