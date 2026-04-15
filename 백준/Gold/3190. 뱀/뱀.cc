52da5c5d823f59674ba4842b17279edf65eda953
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 17:06:42 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,75 +1,75 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// 우, 하, 좌, 상
+// 방향: 우 - 하 - 좌 - 상
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
+int direct;
 int n, k, l;
-int board[104][104];
-map<int, char> direction_change;
+int arr[104][104]; // 사과 위치
+int checked[104][104]; // 뱀 몸 위치 체크
+queue<int> time_queue;
+queue<char> way_queue;
+deque<pair<int, int>> snake; // 뱀 몸 전체 추적
+
+bool check_possible_moving(int x, int y) {
+    if (x >= 1 && x <= n && y >= 1 && y <= n && checked[x][y] == 0) return true;
+    return false;
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+    direct = 0;
     cin >> n >> k;
-    // 사과 위치
+
     for (int i = 0; i < k; i++) {
-        int x, y;
-        cin >> x >> y;
-        board[x][y] = 1; // 사과
+        int r, c;
+        cin >> r >> c;
+        arr[r][c] = 1; // 사과 위치 저장
     }
 
     cin >> l;
     for (int i = 0; i < l; i++) {
-        int t;
-        char d;
-        cin >> t >> d;
-        direction_change[t] = d;
+        int time;
+        char way;
+        cin >> time >> way;
+        time_queue.push(time);
+        way_queue.push(way);
     }
 
-    deque<pair<int, int>> snake;
+    int total_time = 0;
     snake.push_back({1, 1});
-    int dir = 0;
-    int time = 0;
-    vector<vector<bool>> visited(n + 1, vector<bool>(n + 1, false));
-    visited[1][1] = true;
+    checked[1][1] = 1;
 
     while (true) {
-        time++;
-        int nx = snake.front().first + dx[dir];
-        int ny = snake.front().second + dy[dir];
+        total_time++;
+        int nx = snake.front().first + dx[direct];
+        int ny = snake.front().second + dy[direct];
 
-        // 벽이나 자기 자신과 충돌하면 종료
-        if (nx < 1 || ny < 1 || nx > n || ny > n || visited[nx][ny]) {
-            break;
-        }
+        if (!check_possible_moving(nx, ny)) break;
 
-        // 이동
         snake.push_front({nx, ny});
-        visited[nx][ny] = true;
+        checked[nx][ny] = 1;
 
-        if (board[nx][ny] == 1) {
-            // 사과가 있으면 꼬리 안 움직임
-            board[nx][ny] = 0;
+        if (arr[nx][ny] == 1) {
+            arr[nx][ny] = 0;
         } else {
-            // 사과 없으면 꼬리 제거
-            auto tail = snake.back();
-            visited[tail.first][tail.second] = false;
+            checked[snake.back().first][snake.back().second] = 0;
             snake.pop_back();
         }
 
-        // 방향 전환
-        if (direction_change.count(time)) {
-            if (direction_change[time] == 'D') {
-                dir = (dir + 1) % 4;
+        if (!time_queue.empty() && total_time == time_queue.front()) {
+            if (way_queue.front() == 'D') {
+                direct = (direct + 1) % 4;
             } else {
-                dir = (dir + 3) % 4;
+                direct = (direct - 1 + 4) % 4;
             }
+            time_queue.pop();
+            way_queue.pop();
         }
     }
 
-    cout << time << '\n';
+    cout << total_time << '\n';
     return 0;
 }
