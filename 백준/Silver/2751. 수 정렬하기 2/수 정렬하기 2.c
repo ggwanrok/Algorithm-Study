6f48c580e26783a61057dbf3388684b9c9995a29
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:57:58 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 416 ms, Memory: 8804 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -1,38 +1,12 @@
-#include <stdio.h>
-
-#define ALL 2000002                  
-int cheak[ALL] = { 0, };             
-int n, a;
-
-int main()
-{
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%d", &a);
-		if (a < 0)                        
-		{
-			cheak[1000000 - a] = 1;
-		}
-		else                              
-		{
-			cheak[a] = 1;
-		}
-	}
-	for (int i = ALL; i > 1000000; i--)   
-	{
-		if (cheak[i] == 1)
-		{
-			printf("%d\n", 1000000-i);
-		}
-	}
-	for (int i = 0; i < 1000001; i++)     
-	{
-		if (cheak[i] == 1)
-		{
-			printf("%d\n", i);
-		}
-	}
-	return 0;
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
