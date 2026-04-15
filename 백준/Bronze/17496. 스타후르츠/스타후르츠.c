a710a2a8c04197d6ff10223e4cdfbdfb441bd057
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 23:09:52 +0900

    [Bronze III] Title: 스타후르츠, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17496. 스타후르츠/스타후르츠.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+  int t, c, n, p, k;
+  scanf("%d %d %d %d",&n, &t, &c, &p);
+  for(k=0;k<=(n-1)/t;k++){
+   k=k;
+    }
+  
+  printf("%d", (k-1)*c*p);
+  return 0;
+}
