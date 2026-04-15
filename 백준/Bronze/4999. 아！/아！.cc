d7d4e93e63c2d720a641fc7df29aaaacbc642612
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 19:48:39 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string a, b;
+    cin>>a>>b;
+
+    if(a.length() >= b.length()){
+        cout<<"go"<<'\n';
+    }
+    else{
+        cout<<"no"<<'\n';
+    }
+        
+    return 0;
+}
