a756e758a4026b62b99ac7df29a18d0d7bceaa8b
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:05:13 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string a, k; cin>>a>>k;
+    cout<<k;
+    return 0;
+}
