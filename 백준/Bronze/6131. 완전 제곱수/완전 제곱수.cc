c9857808861d64d2d8acb591ec11d7f0f4e95776
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:17:01 +0900

    [Bronze III] Title: 완전 제곱수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.cc
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.cc
@@ -6,10 +6,9 @@ vector <int> v;
 int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n; cin>>n;
-    int a=1, b=1;
     int cnt = 0;
     for(int b=1; b<500; ++b){
-        for(int a=1; a<500; ++a){
+        for(int a=b+1; a<500; ++a){
         if(a*a < b*b +n) continue;
         if(a*a == b*b + n) cnt++;
         }
