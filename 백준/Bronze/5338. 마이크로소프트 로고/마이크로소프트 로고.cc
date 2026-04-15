0ff209c70a2b97632cbea06c8cece59f3594a561
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 19:31:07 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    cout<<"       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|\n";
+
+    return 0;
+}
