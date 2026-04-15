d4b788e96a290867863b3d35e3119dec77c6b69c
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:42:43 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+struct cmp{
+    bool operator()(int a, int b){
+        return a>b;
+    }
+};
+priority_queue<int, vector<int>, cmp> pq;
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    int n; cin>>n;
+    for(int i=0;i<n;++i){
+        int t; cin>>t;
+        if(t==0){
+            if(pq.empty() == 1){
+                cout<<0<<'\n';
+            }
+            else{
+            cout<<pq.top()<<'\n';
+            pq.pop();
+            }
+        }
+        else{
+            pq.push(t);
+        }
+    }
+    return 0;
+}
