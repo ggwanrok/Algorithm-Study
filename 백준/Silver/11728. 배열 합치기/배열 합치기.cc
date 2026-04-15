21fe53a8d6aba43c561a81a3fbda2011b3157f5b
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:09:15 +0900

    [Silver V] Title: 배열 합치기, Time: 556 ms, Memory: 26724 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 vector<int> v;
 vector<int> v2;
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n, m; cin>>n>>m;
     for(int i=0; i<n; ++i){
         int k; cin>>k;
