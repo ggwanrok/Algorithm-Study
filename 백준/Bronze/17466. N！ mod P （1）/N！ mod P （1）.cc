74b693dedcbd43ef26181cc49ae306d46c7eb3b3
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:24:35 +0900

    [Bronze II] Title: N! mod P (1), Time: 1500 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
+++ 백준/Bronze/17466. N！ mod P （1）/N！ mod P （1）.cc
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    long long  n, p; cin>>n>>p;
-    long long  t = 1;
-    for(long long  i=1; i<=n; ++i){
-        t = t *i;
-        if(t>p){
-            t = t%p;
-        }
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int N, P; cin >> N >> P;
+    int res = 1;
+    for(int i=1; i<=N; ++i){
+        res = (res * (long long)i) % P;
+
+        
     }
-    
-    cout<< t%p <<'\n';
-    return 0;
+    cout << res << '\n';
 }
