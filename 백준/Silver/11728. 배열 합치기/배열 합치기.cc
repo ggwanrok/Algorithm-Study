6c4f345de1ad5fac385b99232a0b6a8123ecfa34
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:09:02 +0900

    [Silver V] Title: 배열 합치기, Time: 524 ms, Memory: 26724 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v;
+vector<int> v2;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m; cin>>n>>m;
+    for(int i=0; i<n; ++i){
+        int k; cin>>k;
+        v.push_back(k);
+    }
+    for(int i=0; i<m; ++i){
+        int k; cin>>k;
+        v.push_back(k);
+    }
+    int l =0; int r =n;
+    while(1){
+        if(v[l]<v[r]){
+            v2.push_back(v[l]);
+            l++;
+        }
+        else if(v[r]<v[l]){
+            v2.push_back(v[r]);
+            r++;
+        }
+        else{
+            v2.push_back(v[l]);
+            v2.push_back(v[r]);
+            l++; r++;
+        }
+        if(l == n){
+            for(int i=r; i<v.size(); ++i){
+                v2.push_back(v[i]);
+            }
+            break;
+        }
+        if(r == v.size()){
+            for(int i=l; i<n; ++i){
+                v2.push_back(v[i]);
+            }
+            break;
+        }
+    }
+    for(int i=0; i<v2.size(); ++i){
+        cout<<v2[i]<<" ";
+    }
+
+    return 0;
+}
