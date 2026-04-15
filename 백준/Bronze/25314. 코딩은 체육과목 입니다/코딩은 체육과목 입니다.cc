69589e5bcfefdc37db39651d83ff445f3be0f927
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 16:51:09 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    while(true){
+        if(n <= 0) break;
+        cout<<"long ";
+        n -= 4;
+    }
+    cout<<"int\n";
+    return 0;
+}
