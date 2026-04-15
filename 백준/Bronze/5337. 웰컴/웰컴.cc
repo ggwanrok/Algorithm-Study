09ff444b8e41d7cb2734846e4d29a00627f18878
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 20:29:56 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,16 @@
+//
+// Created by gwanr on 2024-09-02.
+//
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(void){
+    cout<<".  .   ."<<'\n';
+    cout<<"|  | _ | _. _ ._ _  _"<<'\n';
+    cout<<"|/\\|(/.|(_.(_)[ | )(/."<<'\n';
+    return 0;
+}
