f3c85255c316296079d0ba9d5b57a8ffb9aae50c
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 21:58:17 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 int dp[1000001];
-int solve(int x){
-    if(x==1) return 0;
-    if(dp[x] != 0) return dp[x];
-    dp[x] = solve(x-1);
-    if(x%2==0){
-        dp[x] = min(dp[x], solve(x/2));
-    }
-    if(x%3==0){
-        dp[x] = min(dp[x], solve(x/3));
-    }
-    dp[x]++;
-    return dp[x];
-}
 int main(){
     ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
     int n; cin>>n;
-    cout<<solve(n);
+    dp[1] =0;
+    for(int i=2; i<=n; ++i){
+        dp[i] = dp[i-1] +1;
+        if(i%2==0 && dp[i]>dp[i/2]+1){
+            dp[i] = dp[i/2]+1;
+        }
+        if(i%3==0 && dp[i]>dp[i/3]+1){
+            dp[i] = dp[i/3]+1;
+        }
+    }
+    cout<<dp[n];
     return 0;
 }
