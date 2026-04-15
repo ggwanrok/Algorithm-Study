57fe7511263091de6d1ba17dd531b8bbd47cc86a
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 21:23:36 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 700 ms, Memory: 51048 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -9,26 +9,15 @@ int dpPalindrome[2505][2505];
 
 void input();
 void findPalindrome();
-void updatePalindremo(int, int, int);
+void updatePalindrome(int, int);
 void findBest();
 
 int main(void){
     input();
     findPalindrome();
 
-    // for(int i=0; i<=array.size(); i++){
-    //     for(int j=0; j<=array.size(); j++){
-    //         cout<<checkPalindrome[i][j]<<' ';
-    //     }
-    //     cout<<endl;
-    // }
     cout<<dpPalindrome[array.size()][array.size()];
-    // for(int i=1; i<= array.size(); i++){
-    //     for(int j=0; j<= array.size(); j++){
-    //         cout<<dpPalindrome[i][j]<<' ';
-    //     }
-    //     cout<<endl;
-    // }    
+ 
     return 0;
 }
 
@@ -36,41 +25,24 @@ void input(){
     cin>>array;
 }
 
-void updatePalindremo(int start, int end, int OE){
+void updatePalindrome(int start, int end){
     if(end >= array.size()){
         return;
     }
     
-    if(OE == 0){
-        int pCheck = 1;
-        //짝수 길이이다.
-        for(int i=0; i<=(end-start+1)/2; i++){
-            if(array[start + i] != array[end-i]){
-                pCheck = 0;
-                break;
-            }
-        }
-        if(pCheck == 1){
-
-            checkPalindrome[start+1][end+1] = 1;
+    int pCheck = 1;
+    for(int i=0; i<=(end-start+1)/2; i++){
+        if(array[start + i] != array[end-i]){
+            pCheck = 0;
+            break;
         }
     }
-    else{
-        int pCheck = 1;
-        //홀수 길이이다.
-        for(int i=0; i<(end-start+1)/2; i++){
-            if(array[start + i] != array[end-i]){
-                pCheck = 0;
-                break;
-            }
-        }
-        if(pCheck == 1){
+    if(pCheck == 1){
 
-            checkPalindrome[start+1][end+1] = 1;
-        }
-
+        checkPalindrome[start+1][end+1] = 1;
     }
-    updatePalindremo(start, end+1, (end +1 - start + 1) %2);
+        
+    updatePalindrome(start, end+1);
 }
 
 void findBest(){
@@ -80,10 +52,7 @@ void findBest(){
 
     for(int i=1; i<=array.size(); i++){
         for(int j=1; j<=array.size(); j++){
-            //i 와 j의 대소비교를 통해서 값을 갱신.
-            // if(i==j){
-            //     dpPalindrome[i][j] = (dpPalindrome[i][j-1] + 1 < dpPalindrome[i-1][j]) ? dpPalindrome[i][j-1] + 1 : dpPalindrome[i-1][j];
-            // }
+            
             if(i>j){
                 dpPalindrome[i][j] = dpPalindrome[i-1][j];
             }
@@ -112,7 +81,7 @@ void findBest(){
 
 void findPalindrome(){
     for(int i=0; i<array.size(); i++){
-        updatePalindremo(i, i, (i - i + 1)%2);
+        updatePalindrome(i, i);
     }
     
     findBest();
