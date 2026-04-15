d5bbd72c29bc4313682dc5de09b7416652230d18
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 20:57:04 +0900

    [Platinum V] Title: 골목길, Time: 8 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Platinum/1738. 골목길/골목길.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define MAX_DIST 250000001
+using namespace std;
+
+/*
+ * 가중치를 음수로 받아서, 벨만포드를 때러버렸을 때,
+ * 음의 사이클이 있다면, -1을 출력
+ * 아니라면 역추적 진행하기.
+ * 가중치에 대해서 음수로 처리해준 뒤에 단순한 벨만포드를 진행.
+ * 매 업데이트마다 본인이 어디서 업데이트 되었는지만 알아둔다면 되는 문제
+ */
+
+void input();
+bool bellman_ford(int);
+void preMethod(int);
+
+int n, m;
+int route[501];
+int weight[501][501];
+int preRoute[501];
+
+
+int main(){
+    input();
+
+    bool has_minus_cycle = false;
+    has_minus_cycle = bellman_ford(1);
+    if(has_minus_cycle){
+        cout<<"-1";
+    }
+    else{
+        preMethod(n);
+    }
+
+
+    return 0;
+}
+
+void input(){
+    cin>>n>>m;
+
+    for(int i = 1; i <= n; i++) {
+        preRoute[i] =-1;
+        for(int j = 1; j <= n; j++)
+            weight[i][j] = MAX_DIST;
+    }
+
+    for(int i=0; i<m; i++){
+        int s, e, t;
+        cin >> s >> e >> t;
+        weight[s][e] = -t;
+    }
+}
+
+bool bellman_ford(int start) {
+    for(int i = 1; i <= n; i++) route[i] = MAX_DIST;
+    route[start] = 0;
+    for(int i = 1; i <= n; i++) {
+        for(int from = 1; from <= n; from++) {
+            if(route[from] == MAX_DIST) continue;
+            for(int to = 1; to <= n; to++) {
+                if(weight[from][to] == MAX_DIST) continue;
+                if(route[to] > route[from] + weight[from][to]) {
+                    if(i == n && weight[to][n] != MAX_DIST) return true;
+                    route[to] = route[from] + weight[from][to];
+                    preRoute[to] = from;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void preMethod(int s){
+    if(s == 1){
+        cout<<s<<" ";
+    }
+    else{
+        preMethod(preRoute[s]);
+        cout<<s<<" ";
+    }
+}
