d398b16625e71161c1f70dfe95812bdd09d65989
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 21:58:28 +0900

    [Gold V] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int dp[1000001];
+int save[1000001];
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    int n; cin>>n;
+    dp[1] =0;
+    save[1] =0;
+    for(int i=2; i<=n; ++i){
+        dp[i] = dp[i-1] +1;
+        save[i] = i-1;
+        if(i%2==0 && dp[i]>dp[i/2]+1){
+            dp[i] = dp[i/2]+1;
+            save[i] = i/2;
+        }
+        if(i%3==0 && dp[i]>dp[i/3]+1){
+            dp[i] = dp[i/3]+1;
+            save[i] = i/3;
+        }
+    }
+    cout<<dp[n]<<'\n';
+    while(n!=0){
+        cout<<n<<" ";
+        n = save[n];
+    }
+    return 0;
+}
