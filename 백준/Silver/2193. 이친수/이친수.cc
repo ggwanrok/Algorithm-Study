1509c9d4b65be072d971d29e286b39270f1be4dc
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:00:18 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+long long dp[91];
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    int n; cin>>n;
+    dp[1] = 1;
+    dp[2] = 1;
+    for(int i=3; i<=n; ++i){
+        dp[i] = dp[i-2] + dp[i-1];
+    }
+    cout<<dp[n];
+    return 0;
+}
