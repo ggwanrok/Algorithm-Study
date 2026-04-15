bf4f71514f9801553529872fb81ed68268dbbdf2
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:46:54 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 1156 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int check[100];
+int arr[100];
+int mon[10001];
+int k;
+void swap(int* x, int* y) {
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void btk (int j, int n, int m, int i){
+  if(j==m){
+    for(int a=0;a<j;a++){
+        printf("%d ", arr[a]);
+    }
+    printf("\n");
+    }
+  else{
+    for(;i<=n;i++){
+      
+      
+        arr[j]=mon[i];
+       
+        btk(j+1,n,m, i);
+      
+      }
+    }
+  }
+  
+int main(){
+  int n, m;
+  scanf("%d %d", &n, &m);
+  for(int i=1;i<=n;i++){
+    scanf("%d", &mon[i]);
+  }
+  for(int i=1;i<=n;i++){
+    for(int j=i+1;j<=n;j++){
+      if(mon[i]>mon[j]){
+        swap(&mon[i], &mon[j]);
+      }
+    }
+  }
+  btk(0,n,m, 1);
+  return 0;
+}
