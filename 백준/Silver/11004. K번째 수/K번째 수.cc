e8b8383203ab8c375a7a30e24bb71c4e9e3642bf
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 17:07:34 +0900

    [Silver V] Title: K번째 수, Time: 1000 ms, Memory: 51300 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -2,63 +2,19 @@
 
 using namespace std;
 
-priority_queue<int> pq;
+int n, k;
+vector<int> a;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, k;
     cin>>n>>k;
     while(n--){
-        int i;
-        cin>>i;
-        if(pq.size() < k) pq.push(i);
-        else{
-            if(i < pq.top()){
-                pq.pop();
-                pq.push(i);
-            }
-        }
+        int tmp;
+        cin>>tmp;
+        a.push_back(tmp);
     }
-    cout<<pq.top()<<'\n';
+    sort(a.begin(), a.end());
+    cout<<a[k-1]<<'\n';
+
     return 0;
 }
-
-//set
-// int main(){
-//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-//     set<int> s;
-//     int n, k;
-//     int tar = 1;
-//     cin>>n>>k;
-//     while(n--){
-//         int c;
-//         cin>>c;
-//         s.insert(c);
-//     }
-//     for(int curr : s){
-//         if(tar == k){
-//             cout<<curr<<'\n';
-//             break;
-//         }
-//         tar++;
-//     }
-
-//     return 0;
-// }
-
-//실5 라서 그냥 돌려봤지만, 역시나 시간초과.
-// int n, k;
-// vector<int> a;
-
-// int main(){
-//     cin>>n>>k;
-//     while(n--){
-//         int tmp;
-//         cin>>tmp;
-//         a.push_back(tmp);
-//     }
-//     sort(a.begin(), a.end());
-//     cout<<a[k-1]<<'\n';
-
-//     return 0;
-// }
