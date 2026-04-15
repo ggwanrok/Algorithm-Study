dc1225af62a6c66fb859d1b155eb5487e7b8ae75
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 21:49:51 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    long long a, b; cin>>a>>b;
+    cout<<a+b<<'\n';
+    return 0;
+} 
