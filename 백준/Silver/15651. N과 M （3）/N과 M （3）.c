16fe261ece11ba218bfda714abf96a2d2da10e23
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:44:53 +0900

    [Silver III] Title: N과 M (3), Time: 476 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int check[100];
+int arr[100];
+
+void btk (int j, int n, int m, int i){
+  if(j==m){
+    for(int a=0;a<j;a++){
+        printf("%d ", arr[a]);
+    }
+    printf("\n");
+    }
+  else{
+    for(int i=1;i<=n;i++){
+    
+        arr[j]=i;
+        btk(j+1,n,m, i);
+      
+      
+    }
+  }
+  }
+int main(){
+  int n, m;
+  scanf("%d %d", &n, &m);
+  btk(0,n,m, 1);
+  return 0;
+}
