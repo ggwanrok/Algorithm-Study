b27f21898c98069792e668ca6faa5b18213eae39
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 16:32:15 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -5,42 +5,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    /*
+    최대가 한번 등장할 때 : 최댓값을 n이라고 하면 최대 n^2만큼 이동
+    최대가 두번 등장할 때 : 최댓값을 n이라고 하면 최대 n^2 + n만큼 이동
+
+    거리를 계산한 뒤에 해당 거리에 루트를 씌워 몇번의 스텝을 밟아야하는지 구할 수 있다.
+    */
+
     int tc;
     cin >> tc;
-
     while (tc--) {
         long long x, y;
         cin >> x >> y;
+        long long d = y - x;
 
-        long long dist = y - x;
-        if (dist == 0) {
-            cout << 0 << '\n';
-            continue;
-        }
+        long long n = (long long) sqrt(d);
 
-        long long moves = 0;
-        long long covered = 0;
-        long long step = 1;
+        long long ans;
+        if (d == n * n) ans = 2 * n - 1;
+        else if (d <= n * n + n) ans = 2 * n;
+        else ans = 2 * n + 1;
 
-        //걸음걸이마다 도달할 수 있는 최대거리가 존재.
-        //해당 최대거리 이내에 대해서는 해당 보폭으로 해결할 수 있다. (일정한 증감비율로 인해서)
-        //그래서 결과적으로 피라미드식 그래프 형태를 보이는 구조이기 때문에
-        //홀수일 때 탑을 올리고, 짝수일 때 같은 높이를 한번 더 올리는 것까지 가능하고 해당 범위까지 커버가 가능해진다.
-        //커버 가능한 범위 내의 거리가 산정되면 해당 값이 최솟값이다.
-        while (covered < dist) {
-            covered += step;
-            moves++;
-            if (covered >= dist) break;
-
-            covered += step;
-            moves++;
-            if (covered >= dist) break;
-
-            step++;
-        }
-
-        cout << moves << '\n';
+        cout << ans << '\n';
     }
-
     return 0;
 }
