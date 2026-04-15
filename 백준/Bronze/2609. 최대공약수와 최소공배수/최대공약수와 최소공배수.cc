5ec9d6c2e466aadfb04c9356ddbcbd3aa2fe881a
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:41:19 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,16 +1,35 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-int gcd(int a, int b){
-    if(b==0) return a;
-    return gcd(b, a%b);
-}//a,b를 이용한 재귀를 통해서 a로 최대공약수가 리턴된다.
-//최소공배수는 a*b/최대공약수임을 알면 된다.
-int lcm(int a, int b){
-    return a*b/gcd(a, b);
-}
+
+
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int q, w; cin>>q>>w;
-    cout<<gcd(q, w)<<'\n'<<lcm(q, w)<<'\n';
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    vector <int> A;
+    vector <int> B;
+    int a, b; cin>>a>>b;
+    int t=0;
+    int max=0;
+    for(int i=1;i<=a;++i){
+        if(a%i==0){
+            A.push_back(i);
+        }
+    }
+    for(int i=1;i<=b;++i){
+        if(b%i==0){
+            B.push_back(i);
+        }
+    }
+    for(int i=0;i<A.size();++i){
+        for(int j=0;j<B.size();++j){
+            if(A[i] == B[j]){
+                t = A[i];
+            }
+            if(t>=max){
+                max = t;
+            }
+        }
+    }
+    cout<<max<<'\n'<<a*b/max<<'\n';
+    return 0;
 }
