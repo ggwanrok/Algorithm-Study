7c38297bb2299a9cab9a8f9b86b44e4f27a39457
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 22:36:41 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(0);
+    int a, b; cin>>a>>b;
+    cout<<a+b<<'\n'<<a-b<<'\n'<<a*b<<'\n'<<a/b<<'\n'<<a%b<<'\n';
+    return 0;
+}
