99c8a1d50a6a5f8ff7ad4b2b020337a378212122
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 19:36:40 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -18,10 +18,18 @@ int main(){
     n /= 2;
     dp[0] = 0;
     dp[1] = 3;
-    dp[2] = 11;
-    dp[3] = 41;
-    for(int i=4; i<=n; i++){
-        dp[i] = 3*dp[i-1] + 3*dp[i-2] - dp[i-3]; + 2;
+    for(int i=2; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            if(j==1){
+                dp[i] = dp[i] + dp[i-j] * dp[j];
+            }
+            else if(j == i){
+                dp[i] += 2;
+            }
+            else{
+                dp[i] = dp[i] + 2 * dp[i-j];
+            }
+        }
     }
     cout<<dp[n]<<'\n';
     return 0;
