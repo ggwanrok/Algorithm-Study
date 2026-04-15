1b21b567167a11d55e0488bc8546c6dbf4473e12
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 20:01:44 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -8,7 +8,6 @@ ll lev[60];
 
 void fill_lev();
 ll count_1s(ll);
-ll count_1s_rec(ll);
 
 int main(){
     fill_lev();
@@ -19,12 +18,15 @@ int main(){
 
 // 1부터 x까지의 1의 개수를 계산하는 함수
 ll count_1s(ll x) {
-    if (x <= 1) return x; // 0까지의 1의 개수는 0
-    int level = 0;
+    if (x <= 1) return x; 
+    // 이미 1단계 이상.
+
+    int level = 0; //온전히 포함하는 단계를 의미함.
+
     ll i = 2;
 
-    //속한 레벨 찾기.
-    for(; i*2 <= x; i *= 2) level++;
+    //온전히 포함하는 레벨 찾기. + i는 온전히 포함하지 못하는 단계의 최상위비트를 챙김.
+    for(; i*2 <= x; i *= 2) level++; //다음단계를 온전히 표현할 수 있다? -> level ++ 
 
     ll remaining = x - i; // 현재 레벨 내에서 남은 숫자
     //[해당 전까지 사용된 1의 갯수] + [현재 레벨에서 사용될 최상위비트 1의 갯수] + [남은 수들의 1의 갯수]
