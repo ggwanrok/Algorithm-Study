c87f510d85481a1acb57a8df73d6ce671d0167ba
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-15 19:38:31 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    int a = 0;
+    for(int i=0; i<5; i++){
+        int k; cin>>k;
+        a+=k;
+    }
+    cout<<a<<'\n';
+    return 0;
+}
