f08bd249f6163ad902624f84a47569b211a36028
gwanrok <145204556+ggwanrok@users.noreply.github.com>
2026-04-29 16:51:26 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-정수형 자료 타입 안에서 모두 해결 된다.
-결국 에너지를 얼마나 소모해야하는지를 써야 하는 것.
-
-근데 결국 2배씩 커지는 부분이 최대한 많아야하는 것 같은데
-*/
 
 int solution(int n)
 {
     int ans = 0;
-    while(n > 0){
-        if(n % 2 != 0) ans++;
-        n /= 2;
+    // while(n > 0){
+    //     if(n % 2 != 0) ans++;
+    //     n /= 2;
+    // }
+    while(n){
+        if(n & 1) ans++;
+        n = n >> 1;
     }
     
     
